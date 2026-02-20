1956dc21b58154cd8b2bd7f67c39849a2be8e872
alan4213 <alanbenny7@gmail.com>
2026-02-20 23:31:21 +0530

    Fix Django setup in ASGI for production

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
